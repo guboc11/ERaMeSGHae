08d886374b67fd91a4a9af8e92440a82865e592d
charlie141 <charlie141@naver.com>
2024-08-28 23:24:24 +0900

    add deploy.ts

--- contract/deploy/deploy.ts
+++ contract/deploy/deploy.ts
@@ -1,1 +1,17 @@
-//init
+import { ethers } from "hardhat";
+
+async function main() {
+  try {
+    console.log("Start contract deployment");
+    const factory = await ethers.getContractFactory("MyContract");
+    const contract = await factory.deploy();
+    const address = await contract.getAddress();
+
+    console.log(`Contract is deployed : ${address}`);
+  } catch (e) {
+    console.error(e);
+    process.exitCode = 1;
+  }
+}
+
+main();
