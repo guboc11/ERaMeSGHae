d52c0ded9e9052885b0f01143befd60656ab1232
guboc11 <guboc11@gmail.com>
2024-08-28 20:17:51 +0900

    Add createConstruction, createSupervisor funtions & Update testcode

--- contract/contracts/MyContract.sol
+++ contract/contracts/MyContract.sol
@@ -1,47 +1,130 @@
 pragma solidity ^0.8.13;
 
 contract MyContract {
-
-
   struct Construction {
-    string id;
+    uint id;
+    string name;
     BaseBuild baseBuild;
     FramingBuild framingBuild;
     FinishingBuild finishingBuild;
   }
 
+  // 기초 공사
   struct BaseBuild {
-    uint process1;
-    uint process2;
-    uint process3;
+    uint usedShovelHour;
+    uint neededShovelHour;
+    uint usedSand;
+    uint neededSand;
   }
 
+  // 골조 공사
   struct FramingBuild {
-    uint process1;
-    uint process2;
-    uint process3;
+    uint usedSteelFrame;
+    uint neededSteelFrame;
+    uint usedCement;
+    uint neededCement;
   }
 
+  // 마무리 공사
   struct FinishingBuild {
-    uint process1;
-    uint process2;
-    uint process3;
+    uint usedTiles;
+    uint neededTiles;
+    uint usedPipes;
+    uint neededPipes;
+    uint usedGlue;
+    uint neededGlue;
   }
-
 
   struct Supervisor {
-    string id;
-    string constructionId;
-    string buildId;
-    bool result1;
+    uint id;
+    uint constructionId;
+    string buildName;
     string question1;
+    bool result1;
     string rejectReason1;
-    bool result2;
     string question2;
+    bool result2;
     string rejectReason2;
-    bool result3;
     string question3;
+    bool result3;
     string rejectReason3;
   }
+
+  mapping (string => Construction) private constructionMap;
+  mapping (uint => Supervisor) private supervisorMap;
+
+  uint private constructionId = 0;
+  uint private supervisorId = 0;
+
+  function getConstructionId() public returns (uint) {
+    return constructionId++;
+  }
+
+  function getSupervisorId() public returns (uint) {
+    return supervisorId++;
+  }
+
+
+  function createConstruction(
+    string memory name,
+    uint neededShovelHour,
+    uint neededSand,
+    uint neededSteelFrame,
+    uint neededCement,
+    uint neededTiles,
+    uint neededPipes,
+    uint neededGlue
+  ) public {
+    BaseBuild memory newBaseBuild = BaseBuild(0, neededShovelHour, 0, neededSand);
+    FramingBuild memory newFrameBuild = FramingBuild(0, neededSteelFrame, 0, neededCement);
+    FinishingBuild memory newFinishBuild = FinishingBuild(0, neededTiles, 0, neededPipes, 0, neededGlue);
+
+    uint constructionId = getConstructionId();
+
+    Construction memory newConstruction = Construction(
+      constructionId,
+      name,
+      newBaseBuild,
+      newFrameBuild,
+      newFinishBuild
+    );
+
+    constructionMap[newConstruction.name] = newConstruction;
+  }
+
+  function createSupervisor(
+    uint constructionId,
+    string memory buildName,
+    string memory question1,
+    string memory question2,
+    string memory question3
+  ) public {
+    uint supervisorId = getSupervisorId();
+
+    Supervisor memory newSupervisor = Supervisor(
+      supervisorId,
+      constructionId,
+      buildName,
+      question1,
+      false,
+      "",
+      question2,
+      false,
+      "",
+      question3,
+      false,
+      ""
+    );
+
+    supervisorMap[supervisorId] = newSupervisor;
+  }
+
+  function getConstruction(string memory constructionName) public view returns(Construction memory) {
+    Construction memory c = constructionMap[constructionName];
+
+    return c;
+  }
+
+
 
 }
--- contract/test/MyContractTest.ts
+++ contract/test/MyContractTest.ts
@@ -7,8 +7,28 @@ describe("Starbucks", function() {
     const MyContract = await hre.ethers.getContractFactory("MyContract");
     const myContract = await MyContract.deploy();
 
-    console.log(myContract.customer);
-    console.log(myContract.starbucksYonsei);
+    await myContract.createConstruction(
+      "construction1",
+      400,
+      70,
+      200,
+      40,
+      3000,
+      150,
+      5
+    );
+
+    await myContract.createSupervisor(
+      1,
+      "baseBuild",
+      "baseBuild-question1",
+      "baseBuild-question2",
+      "baseBuild-question3",
+    );
+
+    let construction = await myContract.getConstruction("constrtion1");
+    console.log(construction.toString());
+
   }
 
   test();
