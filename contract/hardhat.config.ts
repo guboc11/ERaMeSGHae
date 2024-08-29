40fb87ccfcb7dd5e6f36b4245f43538aafe65521
guboc11 <guboc11@gmail.com>
2024-08-29 13:04:22 +0900

    Allow unlimited contract size true

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -6,6 +6,9 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    }
   },
   solidity: "0.8.13",
 };
